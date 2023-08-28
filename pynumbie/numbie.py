ea7c0ca61722c9c68f67df44c6ed815f84d9bc6b
akaalena <xmkwier@outlook.com>
2023-08-28 15:26:12 +0200

    pynumbie functionality

--- pynumbie/numbie.py
+++ pynumbie/numbie.py
@@ -2,20 +2,19 @@ import argparse
 from random import choice
 
 
-excluded = []
-def random_integer(start, stop, excluded):
-    return choice(list(set(range(start, stop) - set(excluded))))
+def random_integer(start, stop, excluded = []):
+    print(f"start = {start}, stop = {stop}, excluded = {excluded}")
+    return choice(list(set(range(start, stop)) - set(excluded)))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog='PYNUMBIE'
     )
-    parser.add_argument('--s', required=True, type=int, help='Start of the range')
-    parser.add_argument('--e', required=True, type=int, help='End of the range')
-    parser.add_argument('--x', required=False, type=int, help='Number to exclude')
+    parser.add_argument('-s', '--start', required=True, type=int, help='Start of the range')
+    parser.add_argument('-e', '--end', required=True, type=int, help='End of the range')
+    parser.add_argument('-x', '--excluded', nargs='+', required=False, default=[], type=int, help='Number to exclude')
     args = parser.parse_args()
-    excluded.append(args.x)
     
-    number = random_integer(args.s, args.e, excluded)
+    number = random_integer(args.start, args.end, args.excluded)
     print(f"{number}")
